6ddeb92819d9ef788b2df0551e9d31df5bf7d6b7
Arvind Thirumalai <arvindt@wustl.edu>
2019-12-04 19:48:15 -0600

    Updates

---
+++ precache-manifest.b1a2cd0db049ade328446e9085ac2131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "462f8aed862b32f41e7d",
+    "url": "/09-todo-react-arvindthirumalai/static/css/main.b42b32db.chunk.css"
+  },
+  {
+    "revision": "462f8aed862b32f41e7d",
+    "url": "/09-todo-react-arvindthirumalai/static/js/main.462f8aed.chunk.js"
+  },
+  {
+    "revision": "939cbde5f9c76620fa49",
+    "url": "/09-todo-react-arvindthirumalai/static/js/1.939cbde5.chunk.js"
+  },
+  {
+    "revision": "408299de375a86f37f3a",
+    "url": "/09-todo-react-arvindthirumalai/static/js/runtime~main.408299de.js"
+  },
+  {
+    "revision": "c7a30516d2b790c7c8a0a0ed407d3762",
+    "url": "/09-todo-react-arvindthirumalai/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-react/precache-manifest.857ce18bebcba928b3be6d240402ba41.js"
+  "/09-todo-react-arvindthirumalai/precache-manifest.b1a2cd0db049ade328446e9085ac2131.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todo-react/index.html", {
+workbox.routing.registerNavigationRoute("/09-todo-react-arvindthirumalai/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
